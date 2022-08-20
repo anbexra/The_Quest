b8b77fc79f89e01968e2af4ef6ccbd260e33233c
Anabel <anabelexpositoramos@gmail.com>
2022-08-20 21:49:56 +0200

    actualizacion

--- quest/game.py
+++ quest/game.py
@@ -3,30 +3,34 @@ import os
 import pygame as pg
 
 from . import ALTO, ANCHO
-from quest.escenas import Portada, Partida, HallOfFame
+from quest.pantallas import Portada, Partida, HallOfFame
 
 
 class Quest:
     """Arranca el juego"""
     def __init__(self):
         pg.init()
-        self.display = pg.display.set_mode((ANCHO, ALTO))
+        self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         pg.display.set_caption("The Quest")
         #pg.mixer.init()
 
-        self.escenas = [
-            Portada(self.display),
-            Partida(self.display),
-            HallOfFame(self.display)
+        self.pantallas = [
+            Portada(self.pantalla),
+            Partida(self.pantalla),
+            HallOfFame(self.pantalla)
         ]
         
     def jugar(self):
         """Inicia el bucle principal"""
-        for escena in self.escenas:
-            escena.bucle_principal()
+        for pantalla in self.pantallas:
+            pantalla.bucle_principal()
 
 """Creado la pantalla,
-     proximo paso es hacer que se abran las 3 pantallas diferentes
+ poner fondo y nave 
+ nave en movimiento
+ crear meteoritos * ovnis
+ asomar planeta
+ aterrizar nave
 
      no reconoce bucle principal
 """       
--- quest/pantallas.py
+++ quest/pantallas.py
@@ -1,8 +1,11 @@
+import os
 
 import pygame as pg
 
+from . import ALTO, ANCHO, COLOR_FONDO, COLOR_TEXTO
+
 class Escena:
-    def __init__(self, pantalla):
+    def __init__(self, pantalla: pg.Surface):
         self.pantalla = pantalla
         self.reloj = pg.time.Clock()
     
@@ -11,7 +14,13 @@ class Escena:
         pass
 
 
-class Portada(Escena):         
+class Portada(Escena):   
+    def __init__(self, pantalla: pg.Surface):
+        super().__init__(pantalla)
+       # self.texto = #rellenar con ruta del texto
+        font_file = os.path.join("resources", "fonts", "SIXTY.TTF")
+        self.tipografia = pg.font.Font(font_file)
+
     def bucle_principal(self):
         salir = False
         while not salir:
@@ -20,12 +29,24 @@ class Portada(Escena):
                     salir = True
                 if event.type == pg.QUIT:
                     pg.quit()
-            self.pantalla.fill((0,0,0))
+            self.pantalla.fill((COLOR_FONDO))
+
+            self.pintar_cabecera()
 
             pg.display.flip()       
 
+    def pintar_cabecera(self):
+        cabecera = "THE QUEST"
+        texto = self.tipografia.render(cabecera, True, COLOR_TEXTO)
+        ancho_texto = texto.get_width()
+        pos_x = (ANCHO - ancho_texto) / 2
+        pos_y = ALTO / 3
+        self.pantalla.blit(self.texto, (pos_x, pos_y) )
 
 class Partida(Escena):
+    def __init__(self, pantalla: pg.Surface):
+        super().__init__(pantalla)
+
     def bucle_principal(self):
         salir = False
         while not salir:
@@ -34,7 +55,7 @@ class Partida(Escena):
                     salir = True
                 if event.type == pg.QUIT:
                     pg.quit()
-            self.pantalla.fill((0, 0, 0))
+            self.pantalla.fill((COLOR_FONDO))
 
             pg.display.flip()
             
@@ -45,11 +66,8 @@ class HallOfFame(Escena):
         salir = False
         while not salir:
             for event in pg.event.get():
-                if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                    salir = True
                 if event.type == pg.QUIT:
                     pg.quit()
-            self.pantalla.fill((0, 0, 0))
-
+            self.pantalla.fill((11, 11, 11))
             pg.display.flip()
 
